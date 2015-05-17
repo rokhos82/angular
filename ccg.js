28c3c91ff724a691ccfbccaf6b60cf1cfdedcd25
Justin Lane <justin.l.lane@gmail.com>
2015-05-16 20:21:28 -0600

    20150516 - CCS for tables

--- ccg.js
+++ ccg.js
@@ -20,7 +20,7 @@ with (ccg) {
 	tickets.push(new ticket("Kim Hiser","Treasuer Office","ATM is show error codde 0047."));
 	tickets.push(new ticket("Jim Willox","Commission Board","Notification of virus in email from spam filter."));
 	tickets.push(new ticket("Vere Cooper","Sheriff Office","Unable to open reports in Justice!"));
-	tickets.push(new ticket("Ben Peach","Sheriff Office","Unable to use logic in any meaningful manner."));
+	tickets.push(new ticket("Ben Peach","Sheriff Office","Unable to use logic in any meaningful manner.  Even more text in this line to see if the text wrapping.  One if by land; two of by sea."));
 }
 
 ccg.tickets[4].resolved = true;
